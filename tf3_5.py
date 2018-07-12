947cba7a764bca5492f1ba42a9fff54b4a0caeda
Xingkong <690047924@qq.com>
2018-07-12 20:28:15 +0800

    the initial edition

---
+++ tf3_5.py
@@ -0,0 +1,25 @@
+# tf3-5.py
+# NN前向传播
+# 两层简单神经网络（全连接）
+import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# 定义输入和参数
+# 使用placeholder实现输入定义（sess.run喂多组数据）
+x=tf.placeholder(tf.float32,shape=(None,2))
+w1=tf.Variable(tf.random_normal([2,3],stddev=1,seed=1))
+w2=tf.Variable(tf.random_normal([3,1],stddev=1,seed=1))
+
+# 定义前向传播过程
+a=tf.matmul(x,w1)
+y=tf.matmul(a,w2)
+
+# 使用会话计算结果
+with tf.Session() as sess:
+	init_op=tf.global_variables_initializer()
+	sess.run(init_op)
+	print('the result of tf3-5.py is :\n',sess.run(y,feed_dict=
+			{x: [[0.7,0.5],[0.2,0.3],[0.3,0.4],[0.4,0.5]]}))
+	print('w1:\n',sess.run(w1))
+	print('w2:\n',sess.run(w2))
